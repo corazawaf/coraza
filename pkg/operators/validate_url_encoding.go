afd1c5147d36f7a680c2fbe8f0fbc63cbdce267c
Juan Pablo Tosso <jtosso@coraza.tech>
2020-07-31 22:34:19 -0400

    fix reliability

--- pkg/operators/validate_url_encoding.go
+++ pkg/operators/validate_url_encoding.go
@@ -54,20 +54,19 @@ func validateUrlEncoding(input string) int {
         if (input[i] == '%') {
             if i + 2 >= input_length {
                 /* Not enough bytes. */
-                return -3;
-            } else {
-                /* Here we only decode a %xx combination if it is valid,
-                 * leaving it as is otherwise.
-                 */
-                c1 := input[i + 1];
-                c2 := input[i + 2];
+                return -3
+            }
+            /* Here we only decode a %xx combination if it is valid,
+             * leaving it as is otherwise.
+             */
+            c1 := input[i + 1];
+            c2 := input[i + 2];
 
-                if (((c1 >= '0') && (c1 <= '9')) || ((c1 >= 'a') && (c1 <= 'f')) || ((c1 >= 'A') && (c1 <= 'F'))) && (((c2 >= '0') && (c2 <= '9')) || ((c2 >= 'a') && (c2 <= 'f')) || ((c2 >= 'A') && (c2 <= 'F'))) {
-                    i += 3
-                } else {
-                    /* Non-hexadecimal characters used in encoding. */
-                    return -2
-                }
+            if (((c1 >= '0') && (c1 <= '9')) || ((c1 >= 'a') && (c1 <= 'f')) || ((c1 >= 'A') && (c1 <= 'F'))) && (((c2 >= '0') && (c2 <= '9')) || ((c2 >= 'a') && (c2 <= 'f')) || ((c2 >= 'A') && (c2 <= 'F'))) {
+                i += 3
+            } else {
+                /* Non-hexadecimal characters used in encoding. */
+                return -2
             }
         } else {
             i++
