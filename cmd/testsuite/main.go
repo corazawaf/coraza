7ee9e80171ff8c9739e31c285ea41e4e01efce7f
Juan Pablo Tosso <jtosso@coraza.tech>
2020-09-11 18:59:57 -0300

    add license to all files

--- cmd/testsuite/main.go
+++ cmd/testsuite/main.go
@@ -1,8 +1,20 @@
-/*
-This tool is designed to imitate the OWASP CRS WAF testing framework ftw and can be used to automate WAF testing for DevSecOps.
-Coraza WAF Testsuite does not require a web server as it is used as a standalone library, providing better feedback and faster.
-This tool only works with Coraza WAF.
-*/
+// Copyright 2020 Juan Pablo Tosso
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This tool is designed to imitate the OWASP CRS WAF testing framework ftw and can be used to automate WAF testing for DevSecOps.
+// Coraza WAF Testsuite does not require a web server as it is used as a standalone library, providing better feedback and faster.
+// This tool only works with Coraza WAF.
 package main
 
 import (
--- pkg/transformations/transformations.go
+++ pkg/transformations/transformations.go
@@ -1,8 +1,22 @@
+// Copyright 2020 Juan Pablo Tosso
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package transformations
 
 func TransformationsMap() map[string]interface{} {
 	return map[string]interface{}{
-		"base64Decode":       Base64decode,
+		"base64Decode":			Base64decode,
 		"lowercase":          	LowerCase,
 		"removeWhitespace":		RemoveWhitespace,
 		"removeNulls":        	RemoveNulls,
@@ -20,7 +34,7 @@ func TransformationsMap() map[string]interface{} {
 		//"htmlEntityDecode":   HtmlEntityDecode,
 		"cssDecode": 			CssDecode,
 		"jsDecode": 			JsDecode,
-		"cmdLine":          	CmdLine,
+		"cmdLine":				CmdLine,
 		"length":           	Length,
 		"hexEncode":        	HexEncode,
 		"normalizePath":    	NormalisePath,
