c0e5ffe98e52695ca1a0f42cd7ddf4312c6afc47
Juan Pablo Tosso <jtosso@coraza.tech>
2020-09-10 22:05:01 -0300

    many fixes

--- pkg/transformations/base64decode.go
+++ pkg/transformations/base64decode.go
@@ -1,108 +1,121 @@
 package transformations
 import(
-	"encoding/base64"
 )
 
 func Base64decode(data string) string{
-	ndata, err := base64.StdEncoding.DecodeString(data)
-	if err != nil{
+	res := doBase64decode(data)
+	if res == ""{
 		return data
+	}else{
+		return res
 	}
-	return string(ndata)
 }
 
-//func doBase64decode(input string) string {
-//    slen := len(input)
-//    src := input
-//    var j, x, i, n int
-//    p := []byte{}
-//    dst := []byte{}
-//    olen := 0
-//
+func doBase64decode(input string) string {
+    slen := len(input)
+    src := []byte(input)
+    var j, x, i, n int
+    dst := make([]byte, slen)
+	base64_dec_map := []byte{
+	    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+	    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+	    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+	    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+	    127, 127, 127,  62, 127, 127, 127,  63,  52,  53,
+	     54,  55,  56,  57,  58,  59,  60,  61, 127, 127,
+	    127,  64, 127, 127, 127,   0,   1,   2,   3,   4,
+	      5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
+	     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
+	     25, 127, 127, 127, 127, 127, 127,  26,  27,  28,
+	     29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
+	     39,  40,  41,  42,  43,  44,  45,  46,  47,  48,
+	     49,  50,  51, 127, 127, 127, 127, 127,
+	}
 
-//    /* First pass: check for validity and get output length */
-//    for ;i < slen; i++ {
-//        /* Skip spaces before checking for EOL */
-//        x = 0
-//        for i < slen && src[i] == ' '  {
-//            i++
-//            x++
-//        }
+    /* First pass: check for validity and get output length */
+    for ;i < slen; i++ {
+        /* Skip spaces before checking for EOL */
+        x = 0
+        for i < slen && src[i] == ' '  {
+            i++
+            x++
+        }
 
-//        /* Spaces at end of buffer are OK */
-//        if( i == slen ){
-//            break;
-//        }
+        /* Spaces at end of buffer are OK */
+        if( i == slen ){
+            break
+        }
 
-//        if( ( slen - i ) >= 2 && src[i] == '\r' && src[i + 1] == '\n' ){
-//            continue;
-//        }
+        if ( slen - i ) >= 2 && src[i] == '\r' && src[i + 1] == '\n' {
+            continue
+        }
 
-//        if( src[i] == '\n' ){
-//            continue;
-//        }
+        if( src[i] == '\n' ){
+            continue
+        }
 
-//        /* Space inside a line is an error */
-//        if( x != 0 ){
-//            return input
-//        }
-//        j++
-//        if( src[i] == '=' && j > 2 ){
-//            return input
-//        }
+        /* Space inside a line is an error */
+        if( x != 0 ){
+            return input
+        }
+        if src[i] == '=' {
+        	j++
+        	if j > 2{
+        		//ERROR
+            	return input
+        	}
+        }
 
-//        if( src[i] > 127 || base64_dec_map[src[i]] == 127 ){
-//            return input
-//        }
+        if( src[i] > 127 || base64_dec_map[src[i]] == 127 ){
+        	//ERROR
+            return input
+        }
 
-//        if( base64_dec_map[src[i]] < 64 && j != 0 ){
-//            return input
-//        }
-//        n++
-//    }
+        if( base64_dec_map[src[i]] < 64 && j != 0 ){
+        	//ERROR
+            return input
+        }
+        n++
+    }
 
-//    if n == 0 {
-//        *olen = 0;
-//        return( 0 );
-//    }
+    n = ( ( n * 6 ) + 7 ) >> 3
+    n -= j
 
-//    n = ( ( n * 6 ) + 7 ) >> 3;
-//    n -= j;
+    j = 3
+    n = 0
+    x = 0
+    srcc := 0
 
-//    if( dst == NULL || dlen < n ) {
-//        *olen = n;
-//        return input
-//    }
+    dstc := 0
 
-//    j = 3
-//    n = 0
-//    x = 0
-//    p = dst
-//	for ; i > 0; i-- {
-//        if( *src == '\r' || *src == '\n' || *src == ' ' ){
-//			src++
-//            continue
-//        }
+	for ; i > 0; i-- {
+        if( src[srcc] == '\r' || src[srcc] == '\n' || src[srcc] == ' ' ){
+			srcc++
+            continue
+        }
+        if base64_dec_map[src[srcc]] == 64 {
+        	j--
+        }
+        
+        x  = ( x << 6 ) | int( base64_dec_map[src[srcc]] & 0x3F )
+        n++
+        if( n == 4 ) {
+            n = 0
+            if( j > 0 ) {
+            	dst[dstc] = byte(x >> 16)
+            	dstc++
+            }
+            if( j > 1 ) {
+            	dst[dstc] = byte(x >> 8)
+            	dstc++
+            }
+            if( j > 2 ) {
+            	dst[dstc] = byte(x)
+            	dstc++
+            }
+        }
+		srcc++
+    }
 
-//        j -= ( base64_dec_map[*src] == 64 );
-//        x  = ( x << 6 ) | ( base64_dec_map[*src] & 0x3F );
-//        n++
-//        if( n == 4 ) {
-//            n = 0;
-//            if( j > 0 ) {
-//            	p[0] = x >> 16
-//            }
-//            if( j > 1 ) {
-//            	p[1] = x >> 16
-//            }
-//            if( j > 2 ) {
-//            	p[2] = x >> 16
-//            }
-//        }
-//		src++
-//    }
-
-//    olen = p - dst
-
-//    return string(dst)
-//}
+    return string(dst[:dstc])
+}
--- pkg/transformations/utf8_to_unicode.go
+++ pkg/transformations/utf8_to_unicode.go
@@ -34,7 +34,8 @@ func doUtf8ToUnicode(input string) string{
         unicode_len := 0
         d := 0
         var c byte
-        utf := []byte(input[i:])
+        utf := []byte(input[i:])        
+        utf = append(utf, '\x00')
 
         c = utf[0]
 
@@ -108,14 +109,14 @@ func doUtf8ToUnicode(input string) string{
             /* check we have at least three bytes */
             if (bytes_left < 3) {
                 /* check second byte starts with binary 10 */
-                unicode_len = UNICODE_ERROR_CHARACTERS_MISSING;
+                unicode_len = UNICODE_ERROR_CHARACTERS_MISSING
             } else if (((utf[1]) & 0xC0) != 0x80) {
                 /* check third byte starts with binary 10 */
-                unicode_len = UNICODE_ERROR_INVALID_ENCODING;
+                unicode_len = UNICODE_ERROR_INVALID_ENCODING
             } else if (((utf[2]) & 0xC0) != 0x80) {
-                unicode_len = UNICODE_ERROR_INVALID_ENCODING;
+                unicode_len = UNICODE_ERROR_INVALID_ENCODING
             } else {
-                unicode_len = 3;
+                unicode_len = 3
                 count +=6;
                 if (count <= leng) {
                     length := 0
