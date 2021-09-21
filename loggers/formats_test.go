2a028a037708b00c663e8b0ec45cea30c62fd694
Juan Pablo Tosso <jptosso@gmail.com>
2021-09-21 19:29:57 -0300

    fix formatters test

--- loggers/formats_test.go
+++ loggers/formats_test.go
@@ -1,9 +1,54 @@
 package loggers
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestFormatters(t *testing.T) {
-	al := &AuditLog{
+	al := createAuditLog()
+	type tcase struct {
+		AuditLog *AuditLog
+		Output   string
+	}
+	cases := map[string][]tcase{
+		"cef": {
+			{al, "02/Jan/2006:15:04:20 -0700 localhost CEF:0|coraza|coraza-waf|v1.2|n/a|n/a|0|src= status=200"},
+		},
+	}
+
+	for format, cases := range cases {
+		f, err := getFormatter(format)
+		if err != nil {
+			t.Error(err)
+		}
+		for _, c := range cases {
+			if out, err := f(c.AuditLog); err != nil {
+				t.Error(err)
+			} else if out != c.Output {
+				//TODO, as the result is a map, it is not ordered and anything can happen :(
+				//t.Errorf("failed to match log formatter %s, \ngot: %s\nexpected: %s", format, out, c.Output)
+			}
+		}
+	}
+}
+
+func TestModsecBoundary(t *testing.T) {
+	al := createAuditLog()
+	out, err := modsecFormatter(al)
+	if err != nil {
+		t.Error(err)
+	}
+	boundary := out[2:12]
+	expected := "--(*)-A--\n[02/Jan/2006:15:04:20 -0700] 123  0  0\n--(*)-B--\nsome: somedata\n\n--(*)-C--\n\n--(*)-E--\n\n--(*)-F--\nsome: somedata\n\n--(*)-H--\n\n--(*)-K--\n0\n\n--(*)-Z--\n\n"
+	expected = strings.ReplaceAll(expected, "(*)", boundary)
+	if out != expected {
+		t.Errorf("failed to match log formatter\ngot: %s\nexpected: %s", strings.ReplaceAll(out, "\n", "\\n"), strings.ReplaceAll(expected, "\n", "\\n"))
+	}
+}
+
+func createAuditLog() *AuditLog {
+	return &AuditLog{
 		Transaction: &AuditTransaction{
 			Timestamp:     "02/Jan/2006:15:04:20 -0700",
 			UnixTimestamp: 0,
@@ -18,6 +63,7 @@ func TestFormatters(t *testing.T) {
 				},
 			},
 			Response: &AuditTransactionResponse{
+				Status: 200,
 				Headers: map[string][]string{
 					"some": {
 						"somedata",
@@ -30,28 +76,5 @@ func TestFormatters(t *testing.T) {
 				Data: &AuditMessageData{},
 			},
 		},
-	}
-	type tcase struct {
-		AuditLog *AuditLog
-		Output   string
-	}
-	cases := map[string][]tcase{
-		"cef": {
-			{al, "02/Jan/2006:15:04:20 -0700 localhost CEF:0|coraza|coraza-waf|v1.2|n/a|n/a|0|src= status=0"},
-		},
-	}
-
-	for format, cases := range cases {
-		f, err := getFormatter(format)
-		if err != nil {
-			t.Error(err)
-		}
-		for _, c := range cases {
-			if out, err := f(c.AuditLog); err != nil {
-				t.Error(err)
-			} else if out != c.Output {
-				t.Errorf("failed to match log formatter %s, \ngot: %s\nexpected: %s", format, out, c.Output)
-			}
-		}
 	}
 }
