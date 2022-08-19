5b0f01017fbd558218ecc98679c4b624b2e918f5
Anuraag Agrawal <anuraaga@gmail.com>
2022-08-19 09:31:57 +0900

    Add magefile

---
+++ mage.go
@@ -0,0 +1,19 @@
+// Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build ignore
+// +build ignore
+
+// Entrypoint to mage for running without needing to install the command.
+// https://magefile.org/zeroinstall/
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mage"
+)
+
+func main() {
+	os.Exit(mage.Main())
+}
