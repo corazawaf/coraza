eba3c8784f3ecd183f0bae8526a8599c39787421
Matteo Pace <pace.matteo96@gmail.com>
2022-08-26 17:51:52 +0200

    Implements ipMatchFromDataset
    Signed-off-by: Matteo Pace <pace.matteo96@gmail.com>

---
+++ operators/ip_match_from_dataset_test.go
@@ -0,0 +1,39 @@
+// Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package operators
+
+import (
+	_ "fmt"
+	"testing"
+
+	"github.com/corazawaf/coraza/v3"
+)
+
+func TestIpMatchFromDataset(t *testing.T) {
+	addrok := []string{"127.0.0.1", "192.168.0.1", "192.168.0.253"}
+	addrfail := []string{"127.0.0.2", "192.168.1.1"}
+
+	ipm := &ipMatchFromDataset{}
+	opts := coraza.RuleOperatorOptions{
+		Arguments: "test_1",
+		Datasets: map[string][]string{
+			"test_1": {"127.0.0.1", "192.168.0.0/24"},
+		},
+	}
+
+	if err := ipm.Init(opts); err != nil {
+		t.Error("Cannot init ipmatchfromfile operator")
+	}
+	for _, ok := range addrok {
+		if !ipm.Evaluate(nil, ok) {
+			t.Errorf("Invalid result for single CIDR IpMatchFromDataset " + ok)
+		}
+	}
+
+	for _, fail := range addrfail {
+		if ipm.Evaluate(nil, fail) {
+			t.Errorf("Invalid result for single CIDR IpMatchFromDataset" + fail)
+		}
+	}
+}
