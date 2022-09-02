91aaa377d4a61244cf36e4d4dba260706f23a055
Anuraag Agrawal <anuraaga@gmail.com>
2022-09-02 14:59:04 +0900

    Document caveat

--- internal/io/file.go
+++ internal/io/file.go
@@ -31,6 +31,11 @@ func ReadFirstFile(directories []string, filename string) ([]byte, error) {
 }
 
 // OSFS implements fs.FS using methods on os to read from the system.
+// Note that this implementation is not a compliant fs.FS, as they should only
+// accept posix-style, relative paths, but as this is an internal implementation
+// detail, we get the abstraction we need while being able to handle paths as
+// the os package otherwise would.
+// More context in: https://github.com/golang/go/issues/44279
 type OSFS struct{}
 
 func (o OSFS) Open(name string) (fs.File, error) {
