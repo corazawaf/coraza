409625823f53537f674416cad1c26ab6d46bf322
Anuraag Agrawal <anuraaga@gmail.com>
2022-09-14 10:24:10 +0900

    Use iter for pm operator instead of always finding all

--- operators/pm_from_dataset.go
+++ operators/pm_from_dataset.go
@@ -36,18 +36,7 @@ func (o *pmFromDataset) Init(options coraza.RuleOperatorOptions) error {
 }
 
 func (o *pmFromDataset) Evaluate(tx *coraza.Transaction, value string) bool {
-	if tx.Capture {
-		matches := o.matcher.FindAll(value)
-		for i, match := range matches {
-			if i == 10 {
-				return true
-			}
-			tx.CaptureField(i, value[match.Start():match.End()])
-		}
-		return len(matches) > 0
-	}
-	iter := o.matcher.Iter(value)
-	return iter.Next() != nil
+	return pmEvaluate(o.matcher, tx, value)
 }
 
 var _ coraza.RuleOperator = (*pmFromDataset)(nil)
