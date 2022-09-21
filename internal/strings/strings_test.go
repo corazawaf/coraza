efffa22e2178370cd6bb5bc695a540d81c3bceab
Anuraag Agrawal <anuraaga@gmail.com>
2022-09-21 11:52:58 +0900

    Reduce goroutines in stress test for race detector

--- internal/strings/strings_test.go
+++ internal/strings/strings_test.go
@@ -7,7 +7,7 @@ import "testing"
 
 func TestRandomStringConcurrency(t *testing.T) {
 	// Make sure random strings don't crash under high concurrency.
-	for i := 0; i < 100000; i++ {
-		go RandomString(100)
+	for i := 0; i < 5000; i++ {
+		go RandomString(10000)
 	}
 }
