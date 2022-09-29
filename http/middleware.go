5cf787a858d2b2c677f8a7b984f905adaa8a647c
Matteo Pace <pace.matteo96@gmail.com>
2022-09-29 17:56:48 +0200

    excluding middleware from tinygo

--- http/middleware.go
+++ http/middleware.go
@@ -1,6 +1,10 @@
 // Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// Channels and goroutines are not going to work with tinygo
+//go:build !tinygo
+// +build !tinygo
+
 package http
 
 import (
