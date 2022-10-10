a0fae05b963ae0ff791bed12555f178902205fa8
Adrian Cole <adrian@tetrate.io>
2022-10-10 17:40:49 +0800

    Reads the request protocol instead of hacks
    Not sure when, but Go has the correct protocol version now. This removes
    the hack workaround and backfills tests
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- http/http.go
+++ http/http.go
@@ -34,7 +34,7 @@ func processRequest(tx types.Transaction, req *http.Request) (*types.Interruptio
 	}
 
 	var in *types.Interruption
-	// There is no socket access in the request object so we don't know the server client or port
+	// There is no socket access in the request object, so we neither know the server client nor port.
 	tx.ProcessConnection(client, cport, "", 0)
 	tx.ProcessURI(req.URL.String(), req.Method, req.Proto)
 	for k, vr := range req.Header {
