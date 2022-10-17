ed0507e54b6353947b31cc3627b129b9f6350d88
José Carlos Chávez <jcchavezs@gmail.com>
2022-10-17 22:35:43 +0200

    tests: covers pool.

---
+++ internal/sync/pool_tinygo_test.go
@@ -0,0 +1,36 @@
+// Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build tinygo
+// +build tinygo
+
+package sync
+
+import (
+	"testing"
+)
+
+func TestNewPool(t *testing.T) {
+	p := NewPool(func() interface{} {
+		n := int(1)
+		return &n
+	})
+
+	x, ok := p.Get().(*int)
+	if !ok {
+		t.Fatal("failed to cast first got element")
+		return
+	}
+
+	*x = 2
+	p.Put(x)
+
+	y := p.Get()
+	if want, have := x, y; want != have {
+		t.Errorf("unexpected pool value, want %p, have %p", want, have)
+	}
+
+	if want, have := 2, *(y.(*int)); want != have {
+		t.Error("unexpected pool value")
+	}
+}
