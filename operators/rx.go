f5077503f47d0aa759d709d8682aa3077322505b
Juan Pablo Tosso <jptosso@gmail.com>
2022-12-19 23:43:02 -0300

    optimize rx operator (#536)
    * optimize rx operator

--- operators/rx.go
+++ operators/rx.go
@@ -28,21 +28,22 @@ func newRX(options rules.OperatorOptions) (rules.Operator, error) {
 }
 
 func (o *rx) Evaluate(tx rules.TransactionState, value string) bool {
-	match := o.re.FindStringSubmatch(value)
-	if len(match) == 0 {
-		return false
-	}
 
 	if tx.Capturing() {
+		match := o.re.FindStringSubmatch(value)
+		if len(match) == 0 {
+			return false
+		}
 		for i, c := range match {
 			if i == 9 {
 				return true
 			}
 			tx.CaptureField(i, c)
 		}
+		return true
+	} else {
+		return o.re.MatchString(value)
 	}
-
-	return true
 }
 
 func init() {
