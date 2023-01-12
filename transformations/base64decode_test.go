66d72730eab6fe16082bafb2dce0890759e94211
Anuraag Agrawal <anuraaga@gmail.com>
2023-01-12 10:30:48 +0900

    Add infrastructure for and a couple of fuzz tests (#567)

--- transformations/base64decode_test.go
+++ transformations/base64decode_test.go
@@ -3,16 +3,21 @@
 
 package transformations
 
-import "testing"
+import (
+	"bytes"
+	"encoding/base64"
+	"strings"
+	"testing"
+)
+
+var b64DecodeTests = []string{
+	"VGVzdENhc2U=",
+	"P.HNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg==",
+	"VGVzdABDYXNl",
+}
 
 func BenchmarkB64Decode(b *testing.B) {
-	tests := []string{
-		"VGVzdENhc2U=",
-		"P.HNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg==",
-		"VGVzdABDYXNl",
-	}
-
-	for _, tt := range tests {
+	for _, tt := range b64DecodeTests {
 		b.Run(tt, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				_, err := base64decode(tt)
@@ -22,4 +27,25 @@ func BenchmarkB64Decode(b *testing.B) {
 			}
 		})
 	}
+}
+
+func FuzzB64Decode(f *testing.F) {
+	for _, tc := range b64DecodeTests {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, tc string) {
+		data, err := base64decode(tc)
+		// We decode base64 within non-base64 so there is no
+		// error case.
+		if err != nil {
+			t.Error(err)
+		}
+
+		refData, err := base64.StdEncoding.DecodeString(tc)
+		// The standard library decoder will fail on many inputs ours succeeds on, but when
+		// it doesn't and there are no newlines in the input, they should match.
+		if err == nil && !strings.ContainsAny(tc, "\n\r") && !bytes.Equal([]byte(data), refData) {
+			t.Errorf("mismatch with stdlib for input %s", tc)
+		}
+	})
 }
