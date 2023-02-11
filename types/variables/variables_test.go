67e69f372b3f2ce861d98991a7423fcbeca984dd
José Carlos Chávez <jcchavezs@gmail.com>
2023-02-11 12:48:47 +0100

    chore: generates variable map and variable count. (#641)
    * chore: generates variable map and variable count.
    
    * chore: adds codecov exception.
    
    * chore: uses switch in name.
    
    * tests: removes unneded tests.
    
    ---------
    
    Co-authored-by: Juan Pablo Tosso <jptosso@gmail.com>

--- types/variables/variables_test.go
+++ types/variables/variables_test.go
@@ -14,7 +14,6 @@
 package variables
 
 import (
-	"strings"
 	"testing"
 )
 
@@ -24,36 +23,6 @@ func TestNameToVariable(t *testing.T) {
 		_, err := Parse(v)
 		if err != nil {
 			t.Error("failed to test variable " + v)
-		}
-	}
-}
-
-func TestVariableToName(t *testing.T) {
-	if len(rulemap) != len(rulemapRev) {
-		t.Error("rulemap and rulemapRev are not equal")
-	}
-	for i := 0; i < len(rulemap); i++ {
-		v := RuleVariable(i)
-		if v.Name() != rulemap[v] || rulemapRev[v.Name()] != v {
-			t.Errorf("failed to test variable %s (%d)", v.Name(), v)
-		}
-	}
-}
-
-func TestParseVariable(t *testing.T) {
-	for i := 0; i < len(rulemap); i++ {
-		v := RuleVariable(i)
-		name := v.Name()
-		// we create some lowercase cases
-		if i%3 == 0 {
-			name = strings.ToLower(name)
-		}
-		vv, err := Parse(name)
-		if err != nil {
-			t.Errorf("failed to test variable %s", name)
-		}
-		if v != vv {
-			t.Errorf("failed to test variable %s", name)
 		}
 	}
 }
