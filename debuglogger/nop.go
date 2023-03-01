23b9b20253e1fd4b86382146a337411759044f09
José Carlos Chávez <jcchavezs@gmail.com>
2023-03-01 22:02:37 +0100

    chore: small nit in nop event.

--- debuglogger/nop.go
+++ debuglogger/nop.go
@@ -7,13 +7,13 @@ import "fmt"
 
 type NopEvent struct{}
 
-func (NopEvent) Msg(msg string)                                {}
-func (e NopEvent) Str(key, val string) Event                   { return e }
-func (e NopEvent) Err(err error) Event                         { return e }
-func (e NopEvent) Bool(key string, b bool) Event               { return e }
-func (e NopEvent) Int(key string, i int) Event                 { return e }
-func (e NopEvent) Uint(key string, i uint) Event               { return e }
-func (e NopEvent) Stringer(key string, val fmt.Stringer) Event { return e }
+func (NopEvent) Msg(string)                            {}
+func (e NopEvent) Str(string, string) Event            { return e }
+func (e NopEvent) Err(error) Event                     { return e }
+func (e NopEvent) Bool(string, bool) Event             { return e }
+func (e NopEvent) Int(string, int) Event               { return e }
+func (e NopEvent) Uint(string, uint) Event             { return e }
+func (e NopEvent) Stringer(string, fmt.Stringer) Event { return e }
 
 func Nop() Logger {
 	return defaultLogger{level: LogLevelNoLog}
