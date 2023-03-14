10173205d9066124d02ebc9b1d0502d634112882
José Carlos Chávez <jcchavezs@gmail.com>
2023-03-14 21:53:23 +0100

    tests: yet more coverage.

--- auditlog/logger_test.go
+++ auditlog/logger_test.go
@@ -1,6 +1,9 @@
 // Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build !tinygo
+// +build !tinygo
+
 package auditlog
 
 import (
@@ -16,6 +19,12 @@ func TestDefaultWriters(t *testing.T) {
 		} else if w == nil {
 			t.Errorf("invalid %s writer", writer)
 		}
+	}
+}
+
+func TestGetUnknownWriter(t *testing.T) {
+	if _, err := GetWriter("unknown"); err == nil {
+		t.Error("expected error")
 	}
 }
 
