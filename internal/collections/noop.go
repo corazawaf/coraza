348fff4535aa9ad3c73441e80a5847f4e2354b5c
Juan Pablo Tosso <jptosso@gmail.com>
2023-03-28 17:46:58 -0300

    fix(cols): NOOP now implements map

--- internal/collections/noop.go
+++ internal/collections/noop.go
@@ -4,6 +4,8 @@
 package collections
 
 import (
+	"regexp"
+
 	"github.com/corazawaf/coraza/v3/collection"
 	"github.com/corazawaf/coraza/v3/types"
 )
@@ -12,10 +14,34 @@ var Noop collection.Collection = &noop{}
 
 type noop struct{}
 
-func (c *noop) FindAll() []types.MatchData {
-	return nil
+func (c *noop) Get(key string) []string {
+	return []string{}
 }
+
+func (c *noop) FindRegex(key *regexp.Regexp) []types.MatchData {
+	return []types.MatchData{}
+}
+
+func (c *noop) FindString(key string) []types.MatchData {
+	return []types.MatchData{}
+}
+
+func (c *noop) FindAll() []types.MatchData {
+	return []types.MatchData{}
+}
+
+func (c *noop) Add(key string, value string) {
+	c.Set(key, []string{value})
+}
+
+func (c *noop) Set(key string, values []string) {}
+
+func (c *noop) SetIndex(key string, index int, value string) {}
+
+func (c *noop) Remove(key string) {}
 
 func (c *noop) Name() string {
 	return ""
 }
+
+var _ collection.Map = &noop{}
---
+++ testing/engine/non_vars.go
@@ -0,0 +1,40 @@
+// Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package engine
+
+import (
+	"github.com/corazawaf/coraza/v3/testing/profile"
+)
+
+var _ = profile.RegisterProfile(profile.Profile{
+	Meta: profile.Meta{
+		Author:      "jptosso",
+		Description: "Test if the noon variables work",
+		Enabled:     true,
+		Name:        "noons.yaml",
+	},
+	Tests: []profile.Test{
+		{
+			Title: "actions",
+			Stages: []profile.Stage{
+				{
+					Stage: profile.SubStage{
+						Input: profile.StageInput{
+							URI: "/%FFindex.html?test=test1",
+						},
+						Output: profile.ExpectedOutput{
+							TriggeredRules:    []int{4},
+							NonTriggeredRules: []int{3},
+						},
+					},
+				},
+			},
+		},
+	},
+	Rules: `
+SecRule IP:/_/|TIME|USER ".*" "id: 3,  log"
+SecRule &IP "@eq 0" "id: 4,  log, chain"
+	SecRule &IP:/_/ "@eq 0" "log"
+`,
+})
