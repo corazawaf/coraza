b99e85db6123dedd89fb72b0ace0aba9ded3d86b
Juan Pablo Tosso <jptosso@gmail.com>
2023-03-29 09:13:15 -0300

    fallback noop

--- internal/collections/noop.go
+++ internal/collections/noop.go
@@ -4,42 +4,18 @@
 package collections
 
 import (
-	"regexp"
-
 	"github.com/corazawaf/coraza/v3/collection"
 	"github.com/corazawaf/coraza/v3/types"
 )
 
-var Noop collection.Map = &noop{}
+var Noop collection.Collection = &noop{}
 
 type noop struct{}
 
-func (c *noop) Get(key string) []string {
-	return []string{}
-}
-
-func (c *noop) FindRegex(key *regexp.Regexp) []types.MatchData {
-	return []types.MatchData{}
-}
-
-func (c *noop) FindString(key string) []types.MatchData {
-	return []types.MatchData{}
-}
-
 func (c *noop) FindAll() []types.MatchData {
-	return []types.MatchData{}
+	return nil
 }
-
-func (c *noop) Add(key string, value string) {}
-
-func (c *noop) Set(key string, values []string) {}
-
-func (c *noop) SetIndex(key string, index int, value string) {}
-
-func (c *noop) Remove(key string) {}
 
 func (c *noop) Name() string {
 	return ""
 }
-
-var _ collection.Map = &noop{}
