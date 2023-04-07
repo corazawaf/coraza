1f15017a03fe4e4fe0cf9ea4333d53fca36c9dd9
Anuraag Agrawal <anuraaga@gmail.com>
2023-04-07 14:44:47 +0900

    Append into output buffer for removecommentschar (#764)

--- internal/transformations/remove_comments_char.go
+++ internal/transformations/remove_comments_char.go
@@ -3,14 +3,16 @@
 
 package transformations
 
-func removeCommentsChar(data string) (string, error) {
-	value := []byte(data)
+import stringsutil "github.com/corazawaf/coraza/v3/internal/strings"
+
+func removeCommentsChar(value string) (string, error) {
+	res := make([]byte, 0, len(value))
 	for i := 0; i < len(value); {
 		switch {
 		case value[i] == '/' && (i+1 < len(value)) && value[i+1] == '*':
-			value = erase(value, i, 2)
+			i += 2
 		case value[i] == '*' && (i+1 < len(value)) && value[i+1] == '/':
-			value = erase(value, i, 2)
+			i += 2
 		case value[i] == '<' &&
 			(i+1 < len(value)) &&
 			value[i+1] == '!' &&
@@ -18,26 +20,19 @@ func removeCommentsChar(data string) (string, error) {
 			value[i+2] == '-' &&
 			(i+3 < len(value)) &&
 			value[i+3] == '-':
-			value = erase(value, i, 4)
+			i += 4
 		case value[i] == '-' &&
 			(i+1 < len(value)) && value[i+1] == '-' &&
 			(i+2 < len(value)) && value[i+2] == '>':
-			value = erase(value, i, 3)
+			i += 3
 		case value[i] == '-' && (i+1 < len(value)) && value[i+1] == '-':
-			value = erase(value, i, 2)
+			i += 2
 		case value[i] == '#':
-			value = erase(value, i, 1)
+			i += 1
 		default:
-			i++
+			res = append(res, value[i])
+			i += 1
 		}
 	}
-	return string(value), nil
-}
-
-func erase(str []byte, i int, count int) []byte {
-	// TODO There are better algorithms to do this but not today
-	var res []byte
-	res = append(res, str[0:i]...)
-	res = append(res, str[i+count:]...)
-	return res
+	return stringsutil.WrapUnsafe(res), nil
 }
