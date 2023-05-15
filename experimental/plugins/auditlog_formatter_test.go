f9717c496647b1265d1b9e4555896aee09778b68
José Carlos Chávez <jcchavezs@gmail.com>
2023-05-15 11:24:07 +0200

    tests: fixes tinygo failure.

--- experimental/plugins/auditlog_formatter_test.go
+++ experimental/plugins/auditlog_formatter_test.go
@@ -1,6 +1,11 @@
 // Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build !tinygo
+// +build !tinygo
+
+// Not aimed to tinygo as serial writer is a noop writer
+
 package plugins_test
 
 import (
