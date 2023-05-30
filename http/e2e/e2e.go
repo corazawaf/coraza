e589f1a143fe5ccbbe96bb9160f7387087725c3a
Matteo Pace <pace.matteo96@gmail.com>
2023-05-30 13:10:28 +0200

    adds config check logic and comments around configs

--- http/e2e/e2e.go
+++ http/e2e/e2e.go
@@ -12,15 +12,28 @@ import (
 	"time"
 )
 
-// TODO Expected Coraza configs
-// ...
-// ...
-// CORAZA_HOST
-// HTTPBIN_HOST
-// CORAZA_ENVOY
+// External configurable variables
+// CORAZA_ENVOY: Interruptions at response body phase are allowed to return 200 (Instead of 403), but with a body full of null bytes. Defaults to "false".
+// CORAZA_HOST: Main url used to perform requests. Defaults to "localhost:8080".
+// HTTPBIN_HOST: Backend url, used for health checking reasons. Defaults to "localhost:8081".
 
-// CORAZA_ENVOY=true go run .
+// Expected Coraza configs
+/*
+// coraza.conf-recommended with SecRuleEngine On
+Include @demo-conf
+// Unmodified CRS configuration and rules
+Include @crs-setup-demo-conf
+Include @owasp_crs/*.conf
+// Custom rule for Coraza config check (ensuring that these configs are used)
+SecRule &REQUEST_HEADERS:coraza-e2e "@eq 0" "id:100,phase:1,deny,status:424,msg:'Coraza E2E - Missing header'"
+// Custom rules for e2e testing
+SecRule REQUEST_URI "@streq /admin" "id:101,phase:1,t:lowercase,deny"
+SecRule REQUEST_BODY "@rx maliciouspayload" "id:102,phase:2,t:lowercase,deny"
+SecRule RESPONSE_HEADERS::status "@rx 406" "id:103,phase:3,t:lowercase,deny"
+SecRule RESPONSE_BODY "@contains responsebodycode" "id:104,phase:4,t:lowercase,deny"
+*/
 
+const configCheckStatusCode = 424
 const healthCheckTimeout = 15 // Seconds
 
 func main() {
@@ -59,6 +72,11 @@ func main() {
 			name:         "Coraza health check",
 			url:          urlUnfiltered,
 			expectedCode: 200,
+		},
+		{
+			name:         "Coraza config check",
+			url:          urlUnfiltered,
+			expectedCode: configCheckStatusCode,
 		},
 	}
 
@@ -157,12 +175,24 @@ func main() {
 		client := http.Client{}
 		timeout := healthCheckTimeout
 		tick := time.Tick(time.Second)
+
 		for range tick {
-			resp, err := client.Get(healthCheck.url)
+			req, _ := http.NewRequest(http.MethodGet, healthCheck.url, nil)
+			if healthCheck.expectedCode != configCheckStatusCode {
+				//  The default e2e header is not added if we are checking that the expected config is loaded
+				req.Header.Add("coraza-e2e", "ok")
+			}
+			resp, err := client.Do(req)
 			fmt.Printf("[Wait] Waiting for %s. Timeout: %ds\n", healthCheck.url, timeout)
-			if err == nil && resp.StatusCode == http.StatusOK {
-				fmt.Printf("[Ok] Health check successful, got status code %d\n", resp.StatusCode)
-				break
+			if err == nil {
+				if resp.StatusCode == healthCheck.expectedCode {
+					fmt.Printf("[Ok] Check successful, got status code %d\n", resp.StatusCode)
+					break
+				}
+				if healthCheck.expectedCode == configCheckStatusCode {
+					fmt.Printf("[Fail] Configs check failed, got status code %d, expected %d. Please check configs used.\n", resp.StatusCode, healthCheck.expectedCode)
+					os.Exit(1)
+				}
 			}
 			timeout--
 			if timeout == 0 {
@@ -190,6 +220,8 @@ func main() {
 		for k, v := range test.headers {
 			req.Header.Add(k, v)
 		}
+		req.Header.Add("coraza-e2e", "ok")
+
 		res, err := client.Do(req)
 		if err != nil {
 			fmt.Printf("Error: could not do http request: %s\n", err)
