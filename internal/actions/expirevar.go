0c421059aa0a55e5ae296e67cb0c9121929b1cb1
dextermallo <dextermallo@gmail.com>
2023-06-22 12:16:02 +0100

    chore: update doc for exec/expirevar/initcol/log/logdata actions

--- internal/actions/expirevar.go
+++ internal/actions/expirevar.go
@@ -7,6 +7,22 @@ import (
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
 )
 
+// Action Group: Non-disruptive
+//
+// Description:
+// Configures a collection variable to expire after the given time period (in seconds).
+// You should use the `expirevar` with `setvar` action to keep the intended expiration time.
+// The expire time will be reset if they are used on their own (perhaps in a SecAction directive).
+//
+// Example:
+// ```
+//
+//	SecRule REQUEST_COOKIES:JSESSIONID "!^$" "nolog,phase:1,id:114,pass,setsid:%{REQUEST_COOKIES:JSESSIONID}"
+//
+//	SecRule REQUEST_URI "^/cgi-bin/script\.pl" "phase:2,id:115,t:none,t:lowercase,t:normalizePath,log,allow,\
+//		setvar:session.suspicious=1,expirevar:session.suspicious=3600,phase:1"
+//
+// ```
 type expirevarFn struct{}
 
 func (a *expirevarFn) Init(_ plugintypes.RuleMetadata, data string) error {
