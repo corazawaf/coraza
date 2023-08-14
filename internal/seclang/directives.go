89268f4106563f65c6571bef8df9e69697209c58
Juan Pablo Tosso <jptosso@gmail.com>
2023-08-14 23:06:19 +0200

    ci fixes

--- internal/seclang/directives.go
+++ internal/seclang/directives.go
@@ -477,6 +477,7 @@ func directiveSecPersistenceEngine(options *DirectiveOptions) error {
 	if len(options.Opts) == 0 {
 		return errEmptyOptions
 	}
+	options.WAF.Logger.Warn().Msg("Persistence is a experimental feature, be careful.")
 	engine, err := persistence.Get(options.Opts)
 	if err != nil {
 		return err
