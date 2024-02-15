2e4c9a4b3bddb4ad88791707ce1a63922d0cb36f
Gary Stanley <gary@cpanel.net>
2024-02-15 17:33:34 -0600

    Forgot a tab.

--- internal/collections/concat_test.go
+++ internal/collections/concat_test.go
@@ -6,7 +6,7 @@ package collections
 import (
 	"strings"
 	"testing"
-    regexp "github.com/wasilibs/go-re2"
+	regexp "github.com/wasilibs/go-re2"
 
 	"github.com/corazawaf/coraza/v3/types"
 	"github.com/corazawaf/coraza/v3/types/variables"
