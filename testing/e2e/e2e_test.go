31eff8d867b0de02d3a91d19f136cb0eeaed818c
Fionera <fionera@fionera.de>
2024-03-05 05:06:55 +0100

    feat: expose expected directives for e2e test

--- testing/e2e/e2e_test.go
+++ testing/e2e/e2e_test.go
@@ -23,25 +23,8 @@ import (
 func TestE2e(t *testing.T) {
 	conf := coraza.NewWAFConfig()
 
-	customE2eDirectives := `
-	SecRuleEngine On
-	SecRequestBodyAccess On
-	SecResponseBodyAccess On
-	SecResponseBodyMimeType application/json
-	# Custom rule for Coraza config check (ensuring that these configs are used)
-	SecRule &REQUEST_HEADERS:coraza-e2e "@eq 0" "id:100,phase:1,deny,status:424,log,msg:'Coraza E2E - Missing header'"
-	# Custom rules for e2e testing
-	SecRule REQUEST_URI "@streq /admin" "id:101,phase:1,t:lowercase,log,deny"
-	SecRule REQUEST_BODY "@rx maliciouspayload" "id:102,phase:2,t:lowercase,log,deny"
-	SecRule RESPONSE_HEADERS:pass "@rx leak" "id:103,phase:3,t:lowercase,log,deny"
-	SecRule RESPONSE_BODY "@contains responsebodycode" "id:104,phase:4,t:lowercase,log,deny"
-	# Custom rules mimicking the following CRS rules: 941100, 942100, 913100
-	SecRule ARGS_NAMES|ARGS "@detectXSS" "id:9411,phase:2,t:none,t:utf8toUnicode,t:urlDecodeUni,t:htmlEntityDecode,t:jsDecode,t:cssDecode,t:removeNulls,log,deny"
-	SecRule ARGS_NAMES|ARGS "@detectSQLi" "id:9421,phase:2,t:none,t:utf8toUnicode,t:urlDecodeUni,t:removeNulls,multiMatch,log,deny"
-	SecRule REQUEST_HEADERS:User-Agent "@pm grabber masscan" "id:9131,phase:1,t:none,log,deny"
-`
 	conf = conf.
-		WithDirectives(customE2eDirectives)
+		WithDirectives(e2e.Directives)
 
 	waf, err := coraza.NewWAF(conf)
 	if err != nil {
