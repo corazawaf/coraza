312401187e69972edd1a010a1465a05c9aa42cab
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2024-04-22 18:20:50 +0530

    Add Base64DecodeExt Support

--- internal/transformations/base64decodeext.go
+++ internal/transformations/base64decodeext.go
@@ -1,5 +1,6 @@
 // Copyright 2024 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
+// Decodes a Base64-encoded string. Unlike base64Decode, this version uses a forgiving implementation, which ignores invalid characters.
 
 package transformations
 
