22fefd70cc5ab57547a0ea1a967f178cf3747cda
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2024-04-22 18:38:55 +0530

    Remove go.sum file changes

--- internal/transformations/base64decodeext.go
+++ internal/transformations/base64decodeext.go
@@ -1,9 +1,9 @@
 // Copyright 2024 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
-// Decodes a Base64-encoded string. Unlike base64Decode, this version uses a forgiving implementation, which ignores invalid characters.
-
 package transformations
 
+// Decodes a Base64-encoded string. Unlike base64Decode,
+// this version uses a forgiving implementation, which ignores invalid characters such as whitespace and ".",
 func base64decodeext(data string) (string, bool, error) {
 	res := doBase64decode(data, true)
 	return res, true, nil
