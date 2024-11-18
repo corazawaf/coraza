c5606edd94a139b2434478b9da220fafa57ada75
Matteo Pace <pace.matteo96@gmail.com>
2024-11-18 22:35:16 +0100

    fix: removes multiline from default regex modifiers (#876)
    * removes multiline from default regex modifiers
    
    * relies on coraza.rule.no_regex_multiline
    
    * adds coraza.rule.no_regex_multiline to TagsMatrix
    
    ---------
    
    Co-authored-by: Juan Pablo Tosso <jptosso@gmail.com>

--- internal/operators/rx_no_multiline_test.go
+++ internal/operators/rx_no_multiline_test.go
@@ -1,13 +1,12 @@
 // Copyright 2022 Juan Pablo Tosso and the OWASP Coraza contributors
 // SPDX-License-Identifier: Apache-2.0
 
-//go:build !coraza.rule.no_regex_multiline
+//go:build coraza.rule.no_regex_multiline
 
 package operators
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
@@ -57,22 +56,42 @@ func TestRx(t *testing.T) {
 			want:    true,
 		},
 		{
-			// Requires multiline
+			// Requires multiline disabled by default
 			pattern: `^hello.*world`,
+			input:   "test\nhello\nworld",
+			want:    false,
+		},
+		{
+			// Makes sure multiline can be enabled by the user
+			pattern: `(?m)^hello.*world`,
 			input:   "test\nhello\nworld",
 			want:    true,
 		},
 		{
-			// Makes sure, (?sm) passed by the user does not
+			// Makes sure, (?s) passed by the user does not
 			// break the regex.
-			pattern: `(?sm)hello.*world`,
+			pattern: `(?s)hello.*world`,
 			input:   "hello\nworld",
 			want:    true,
 		},
 		{
 			// Make sure user flags are also applied
-			pattern: `(?i)^hello.*world`,
-			input:   "test\nHELLO\nworld",
+			pattern: `(?i)hello.*world`,
+			input:   "testHELLO\nworld",
+			want:    true,
+		},
+		{
+			// The so called DOLLAR_ENDONLY modifier in PCRE2 is meant to tweak the meaning of dollar '$'
+			// so that it matches only at the very end of the string (see: https://www.pcre.org/current/doc/html/pcre2pattern.html#SEC6)
+			// It seems that re2 already behaves like that by default.
+			pattern: `123$`,
+			input:   "123\n",
+			want:    false,
+		},
+		{
+			// Dollar endonly match
+			pattern: `123$`,
+			input:   "test123",
 			want:    true,
 		},
 	}
@@ -95,29 +114,6 @@ func TestRx(t *testing.T) {
 			if res != tt.want {
 				t.Errorf("want %v, got %v", tt.want, res)
 			}
-			/*
-				vars := tx.GetCollection(variables.TX).Data()
-				if vars["0"][0] != "somedata" {
-					t.Error("rx1 failed")
-				}
-				if vars["1"][0] != "eda" {
-					t.Error("rx1 failed")
-				}
-			*/
 		})
 	}
-}
-
-func BenchmarkRxSubstringVsMatch(b *testing.B) {
-	str := "hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;hello world; heelloo Woorld; hello; heeeelloooo wooooooorld;"
-	rx := regexp.MustCompile(`((h.*e.*l.*l.*o.*)|\d+)`)
-	b.Run("Find all RX", func(b *testing.B) {
-		rx.FindStringSubmatch(str)
-	})
-	b.Run("Find only first", func(b *testing.B) {
-		rx.MatchString(str)
-	})
-	b.Run("Find only N", func(b *testing.B) {
-		rx.FindAllString(str, 3)
-	})
 }
