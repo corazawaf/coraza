224ea9994f538a17c71d9316bd41f496d5e3c895
Roman Suvorov <rsuvorov@cloudlinux.com>
2024-12-23 17:05:10 +0100

    with commented code

--- internal/transformations/transformations.go
+++ internal/transformations/transformations.go
@@ -34,6 +34,7 @@ func init() {
 	Register("compressWhitespace", compressWhitespace)
 	Register("cssDecode", cssDecode)
 	Register("escapeSeqDecode", escapeSeqDecode)
+	// Register("hexDecode", hexDecode)
 	Register("hexEncode", hexEncode)
 	Register("htmlEntityDecode", htmlEntityDecode)
 	Register("jsDecode", jsDecode)
