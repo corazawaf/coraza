c7b3a59666a6fa28c5b54cc3881e5e86bbfd0b85
Roman Suvorov <tty2.rs@gmail.com>
2024-12-31 10:15:16 +0100

    default engine SetTTL test

--- internal/persistence/default_test.go
+++ internal/persistence/default_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 func TestDefaultEngineSetAndGet(t *testing.T) {
+	t.Parallel()
 	engine := &defaultEngine{ttl: int(time.Now().Add(10 * time.Minute).Unix())}
 	err := engine.Set("testCol", "testColKey", "testKey", "testValue")
 	if err != nil {
@@ -79,6 +80,7 @@ func TestDefaultEngineSetAndGet(t *testing.T) {
 }
 
 func TestDefaultGC(t *testing.T) {
+	t.Parallel()
 	engine := &defaultEngine{}
 	engine.Open("", 1)   //nolint:errcheck
 	defer engine.Close() //nolint:errcheck
@@ -100,6 +102,7 @@ func TestDefaultGC(t *testing.T) {
 }
 
 func TestDefaultAll(t *testing.T) {
+	t.Parallel()
 	engine := &defaultEngine{}
 	engine.Open("", 3600) //nolint:errcheck
 	defer engine.Close()  //nolint:errcheck
@@ -113,4 +116,75 @@ func TestDefaultAll(t *testing.T) {
 	if all["testKey"] != "testValue" {
 		t.Errorf("All failed: %v", all)
 	}
+}
+
+func TestSetTTL(t *testing.T) {
+	t.Parallel()
+
+	t.Run("set value then set ttl", func(t *testing.T) {
+		t.Parallel()
+
+		e := &defaultEngine{}
+		err := e.Open("", 3600)
+		if err != nil {
+			t.Errorf("error should be nil, got %v", err)
+		}
+		defer e.Close() //nolint:errcheck
+		if err := e.Set("testCol", "testColKey", "testKey", "testValue"); err != nil {
+			t.Errorf("set failed: %v", err)
+		}
+
+		data := e.getCollection("testCol", "testColKey")
+		_, ok := data["TTL_SET"]
+		if ok {
+			t.Error("TTL_SET should not exist in data map")
+		}
+		to, ok := data["TIMEOUT"]
+		if !ok {
+			t.Error("TIMEOUT should be set")
+		}
+		timeout, ok := to.(int)
+		if !ok {
+			t.Error("TIMEOUT should have type assertion to int")
+		}
+		now := int(time.Now().Unix())
+		if timeout < now && timeout > now+e.ttl+1 {
+			t.Errorf("timeout should be between %d and %d, got %d",
+				now-1, now+e.ttl+1, timeout)
+		}
+
+		newTTL := 6000
+		err = e.SetTTL("testCol", "testColKey", "testKey", newTTL)
+		if err != nil {
+			t.Errorf("error should be nil, got %v", err)
+		}
+
+		// same check after SetTTL
+		data = e.getCollection("testCol", "testColKey")
+		ttlSet, ok := data["TTL_SET"]
+		if !ok {
+			t.Error("TTL_SET should exist in data map")
+		}
+		isTTLSet, ok := ttlSet.(bool)
+		if !ok {
+			t.Error("TTL_SET should have type assertion to bool")
+		}
+		if !isTTLSet {
+			t.Error("TTL_SET should should be true")
+		}
+		to, ok = data["TIMEOUT"]
+		if !ok {
+			t.Error("TIMEOUT should be set")
+		}
+		timeout, ok = to.(int)
+		if !ok {
+			t.Error("TIMEOUT should have type assertion to int")
+		}
+		now = int(time.Now().Unix())
+		if timeout < now && timeout > now+newTTL+1 {
+			t.Errorf("timeout should be between %d and %d, got %d",
+				now-1, now+newTTL+1, timeout)
+		}
+
+	})
 }
