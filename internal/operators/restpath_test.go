d9f23ee2e71bd1d10c9814039d0b4ab32ba30dc3
Nathan Byrd <techy1@gmail.com>
2025-01-03 22:53:39 +0000

    Created failing test for issue

--- internal/operators/restpath_test.go
+++ internal/operators/restpath_test.go
@@ -25,6 +25,25 @@ func TestRestPath(t *testing.T) {
 		t.Errorf("Expected %s to match %s", exp, path)
 	}
 	if tx.Variables().ArgsPath().Get("id")[0] != "123" {
-		t.Errorf("Expected 123, got %s", tx.Variables().ArgsPath().Get("id"))
+		t.Errorf("Expected id to be 123, got %s", tx.Variables().ArgsPath().Get("id"))
+	}
+}
+
+func TestRestPathQueryShouldNotBeIncluded(t *testing.T) {
+	waf := corazawaf.NewWAF()
+	tx := waf.NewTransaction()
+	exp := "/some-random/url/{id}"
+	path := "/some-random/url/123?name=foo"
+	rp, err := newRESTPath(plugintypes.OperatorOptions{
+		Arguments: exp,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if !rp.Evaluate(tx, path) {
+		t.Errorf("Expected %s to match %s", exp, path)
+	}
+	if tx.Variables().ArgsPath().Get("id")[0] != "123" {
+		t.Errorf("Expected id value of 123, got %s", tx.Variables().ArgsPath().Get("id"))
 	}
 }
