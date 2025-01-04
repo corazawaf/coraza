497cac1d5d51d335e13cc7cf008f8072c77d16e6
Nathan Byrd <techy1@gmail.com>
2025-01-04 00:02:03 +0000

    Update regex to prevent greedy matching in REST path and add corresponding test

--- internal/operators/restpath_test.go
+++ internal/operators/restpath_test.go
@@ -47,3 +47,30 @@ func TestRestPathQueryShouldNotBeIncluded(t *testing.T) {
 		t.Errorf("Expected id value of 123, got %s", tx.Variables().ArgsPath().Get("id"))
 	}
 }
+
+func TestRestPathQueryShouldNotBeGreedy(t *testing.T) {
+	waf := corazawaf.NewWAF()
+	tx := waf.NewTransaction()
+
+	exp := "/some-random/url/{id}"
+	testCases := map[string]string{
+		"/some-random/url/123?q=test": "123", // ?q=test is query info
+		"/some-random/url/456/test":   "456", // /test is extra path info
+	}
+
+	for path, want := range testCases {
+
+		rp, err := newRESTPath(plugintypes.OperatorOptions{
+			Arguments: exp,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+		if !rp.Evaluate(tx, path) {
+			t.Errorf("Expected %s to match %s", exp, path)
+		}
+		if tx.Variables().ArgsPath().Get("id")[0] != want {
+			t.Errorf("Expected id value of %s, got %s", want, tx.Variables().ArgsPath().Get("id"))
+		}
+	}
+}
