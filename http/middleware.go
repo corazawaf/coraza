2a43819d236e81ebdf0b514de846b8a25c1015a3
José Carlos Chávez <jcchavezs@gmail.com>
2025-06-26 23:13:44 +0200

    Revert "chore: add tx to context" (#1378)
    Revert "chore: add tx to context (#1345)"
    
    This reverts commit 3fa2318e05cd99f927a12ac7a1488e146c581976.

--- http/middleware.go
+++ http/middleware.go
@@ -8,7 +8,6 @@
 package http
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -116,10 +115,6 @@ func WrapHandler(waf coraza.WAF, h http.Handler) http.Handler {
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		tx := newTX(r)
-
-		ctx := NewContext(r.Context(), &tx)
-		r = r.WithContext(ctx)
-
 		defer func() {
 			// We run phase 5 rules and create audit logs (if enabled)
 			tx.ProcessLogging()
@@ -174,16 +169,4 @@ func obtainStatusCodeFromInterruptionOrDefault(it *types.Interruption, defaultSt
 		return statusCode
 	}
 	return defaultStatusCode
-}
-
-type corazaWafContextKeyType string
-
-const contextTransactionKey = corazaWafContextKeyType("tx")
-
-func TxFromContext(ctx context.Context) *types.Transaction {
-	return ctx.Value(contextTransactionKey).(*types.Transaction)
-}
-
-func NewContext(ctx context.Context, tx *types.Transaction) context.Context {
-	return context.WithValue(ctx, contextTransactionKey, tx)
 }
