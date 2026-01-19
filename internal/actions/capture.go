78d2176c0d02b632fd1125ccac1491e5ae50c1c4
Felipe Zipitria <felipe.zipitria@owasp.org>
2026-01-19 18:56:40 -0300

    fix: go fmt
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- internal/actions/capture.go
+++ internal/actions/capture.go
@@ -21,8 +21,10 @@ import (
 //
 // Example:
 // ```
-// SecRule REQUEST_BODY "^username=(\w{25,})" "phase:2,capture,t:none,chain,id:105"
-// 	SecRule TX:1 "(?:(?:a(dmin|nonymous)))"
+//
+//	  SecRule REQUEST_BODY "^username=(\w{25,})" "phase:2,capture,t:none,chain,id:105"
+//		   SecRule TX:1 "(?:(?:a(dmin|nonymous)))"
+//
 // ```
 type captureFn struct{}
 
