27979790f671a4673c89eb394320c0c378160221
kabbohus <hussein.kabbout@hotmail.com>
2026-07-21 11:48:31 +0200

    Fill ARGS_POST variable even if body bytes were invalid JSON when JSON body processor is running (#1615)
    * Fill ARGS_POST variable even if body bytes were invalid JSON when JSON body processor is running
    
    * Add e2e test to check if process ProcessPartial and JSON body processor can work together
    
    * Typo
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Return gjson error instead of silently dropping it
    
    Signed-off-by: kabbohus <hussein.kabbout@united-security-providers.ch>
    
    * Add comments for more clearance
    
    * Fix wrong author
    
    * Uncomment rule because we expect REQBODY_ERROR AND ARGS_POST to be set
    
    * Remove obsolete comment
    
    * tests: adds unit coverage to json body processor
    
    ---------
    
    Signed-off-by: kabbohus <hussein.kabbout@united-security-providers.ch>
    Co-authored-by: kabbohus <hussein.kabbout@united-security-providers.ch>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: jcchavezs <josecarlos.chavez@owasp.org>
    Co-authored-by: José Carlos Chávez <jcchavezs@gmail.com>

--- testing/engine/json.go
+++ testing/engine/json.go
@@ -74,3 +74,88 @@ SecRule ARGS:json.test3 "@eq 3" "id: 1010, phase:2, log, block"
 SecRule RESPONSE_ARGS:json.test4 "@eq 3" "id: 1011, phase:4, log, block"
 `,
 })
+
+var _ = profile.RegisterProfile(profile.Profile{
+	Meta: profile.Meta{
+		Author:      "kabbohus",
+		Description: "Test if truncated JSON request/response body work",
+		Enabled:     true,
+		Name:        "truncatedjsonyaml",
+	},
+	Tests: []profile.Test{
+		{
+			Title: "json",
+			Stages: []profile.Stage{
+				{
+					Stage: profile.SubStage{
+						Input: profile.StageInput{
+							URI:    "/index.php?json.test=456",
+							Method: "POST",
+							Headers: map[string]string{
+								"content-type": "application/json",
+							},
+							Data: `{"test":123, "test2": 456, "test3": [22, 44, 55]}`,
+						},
+						Output: profile.ExpectedOutput{
+							TriggeredRules: []int{
+								100,
+								101,
+								1100,
+								1101,
+								1104,
+								1010,
+								1000,
+								1011,
+								1111,
+								1200,
+								1201,
+							},
+							NonTriggeredRules: []int{
+								103,
+								1102,
+								1103,
+								1202,
+								1203,
+							},
+							Headers: map[string]string{
+								"Content-Type": "application/json",
+							},
+							Data: `{"test": 123, "test2": 456, "test3": [22, 44, 55], "test4": 3}`,
+						},
+					},
+				},
+			},
+		},
+	},
+	Rules: `
+SecRequestBodyAccess On
+SecResponseBodyAccess On
+SecResponseBodyMimeType application/json
+SecRequestBodyLimitAction ProcessPartial
+SecRequestBodyLimit 40
+SecRule REQUEST_HEADERS:content-type "application/json" "id: 100, phase:1, pass, log, ctl:requestBodyProcessor=JSON"
+SecRule RESPONSE_HEADERS:content-type "application/json" "id:1000, phase:3, pass, log, ctl:responseBodyProcessor=JSON"
+SecRule REQBODY_PROCESSOR "JSON" "id: 101,phase:2,log,block"
+
+SecRule REQBODY_ERROR "!@eq 0" "id:1111, phase:2, log, block"
+
+SecRule REQUEST_BODY "456" "id:103, phase:2, log"
+SecRule ARGS:json.test "@eq 123" "id:1100, phase:2, log, block"
+SecRule ARGS:json.test3.0 "@eq 22" "id:1101, phase:2, log, block"
+SecRule ARGS:json.test3.1 "@eq 44" "id:1102, phase:2, log, block"
+SecRule ARGS:json.test3.2 "@eq 55" "id:1103, phase:2, log, block"
+
+# Both GET and POST can be matched for the same key
+SecRule ARGS:json.test "@eq 456" "id:1104, phase:2, log, block"
+
+SecRule ARGS:json.test3 "@eq 1" "id: 1010, phase:2, log, block"
+
+# Check ARGS_POST
+SecRule ARGS_POST:json.test "@eq 123" "id:1200, phase:2, log, block"
+SecRule ARGS_POST:json.test3.0 "@eq 22" "id:1201, phase:2, log, block"
+SecRule ARGS_POST:json.test3.1 "@eq 44" "id:1202, phase:2, log, block"
+SecRule ARGS_POST:json.test3.2 "@eq 55" "id:1203, phase:2, log, block"
+
+SecRule RESPONSE_ARGS:json.test4 "@eq 3" "id: 1011, phase:4, log, block"
+`,
+})
